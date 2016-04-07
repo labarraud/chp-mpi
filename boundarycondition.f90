30d49a0a965886d083be4e611a9c271da3b9b5c1
Cle-md <delibes.clement@gmail.com>
2016-04-07 11:05:26 +0200

    create subroutine test

--- boundarycondition.f90
+++ boundarycondition.f90
@@ -59,6 +59,48 @@ contains
     end do
   end subroutine cree_tableau_pas
 
+  subroutine test_boundary_conditions()
+    implicit none
+    real*8 ,dimension(:),allocatable::X,Y
+    real*8,dimension(:),allocatable::F,G,H,U,var
+    real*8::Lx,Ly,dt,D
+    integer::Nx,Ny
 
+    call cree_tableau_pas(X,0.0d0,1.0d0,0.1d0)
+    call cree_tableau_pas(Y,0.0d0,1.0d0,0.1d0)
+
+    allocate(F(size(X)*size(Y)))
+    allocate(G(size(X)*size(Y)))
+    allocate(H(size(X)*size(Y)))
+    allocate(U(size(X)*size(Y)))
+
+    D=1.
+    F=0.
+    ! G=F
+    ! H=F
+    G=0.
+    H=1.
+    U=0.
+    dt=12.
+    Lx=1.0
+    Ly=1.0
+    Nx=12
+    Ny=12
+
+    allocate(var(Nx*Nx))
+
+    var=createRHS(G,U,F,H,D,Lx,Ly,Nx,Ny,dt)
+
+
+    ! print*,'X=',X
+    ! print*,'Y=',Y
+    ! print*,'F=',F
+    ! print*,'G=',G
+    ! print*,'H=',H
+    ! print*,'U=',U
+
+
+    print*,'RHS=',var
+  end subroutine test_boundary_conditions
 end module boundarycondition
 
